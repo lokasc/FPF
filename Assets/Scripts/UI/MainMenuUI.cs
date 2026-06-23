9dbec36f114bb16e1e4e4ebfe112245d718175bc
lokasc <lucaschan2012his@gmail.com>
2026-06-23 12:51:24 +0800

    Functioning steam p2p

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using FishNet;
+using UnityEngine.UI;
+using TMPro;
+using Steamworks;
+public class MainMenuUI : MonoBehaviour
+{
+    
+    [SerializeField]private TextMeshProUGUI steamID;
+    [SerializeField]private TextMeshProUGUI steamDisplayName;
+    [SerializeField]private Button hostButton;
+    [SerializeField]private Button clientButton;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // need the guard   
+        clientButton.onClick.AddListener(OnClickClient);
+        hostButton.onClick.AddListener(OnClickHost);
+        
+        steamID.text += " " + SteamClient.SteamId.ToString();
+        steamDisplayName.text += " " + SteamClient.Name.ToString();
+    }
+
+    public void OnClickClient()
+    {
+        InstanceFinder.ClientManager.StartConnection();
+    }
+
+    public void OnClickHost()
+    {
+        InstanceFinder.ServerManager.StartConnection();
+        InstanceFinder.ClientManager.StartConnection();
+    }
+}
